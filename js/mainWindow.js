868f5067a54e80e76cf722dc34c1ee17b657dc39
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-03 02:31:06 +0200

    updated bgs, hide scrollbar and fixed typography aswell as added fullscreen mode

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -11,4 +11,27 @@ function start() {
 	ipcRenderer.on("directoryTo:path", function(e, path){
 		
 	});
+
+	const fullScreen = document.querySelector("#fullScreen");
+	fullScreen.addEventListener("click", fullScreenMode);
+}
+
+let isFullScreen = false;
+function fullScreenMode() {
+	const sideCont =  document.querySelector("#sideCont");
+	const mainCont =  document.querySelector("#mainCont");
+
+	if (isFullScreen === false) {
+		sideCont.className = "col s12";
+		mainCont.style.display = "none";
+		isFullScreen = true;
+		return;
+	}
+
+	else {
+		sideCont.className = "col s3";
+		mainCont.style.display = "block";
+		isFullScreen = false;
+		return;
+	}
 }
