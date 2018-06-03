40ed1d77a82e3f81f2546d974385a015a9edfe34
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-03 04:22:45 +0200

    changed title icon colors, and upated screenshots

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -16,8 +16,8 @@ function start() {
 	const fullScreen = document.querySelector("#fullScreen");
 	fullScreen.addEventListener("click", fullScreenMode);
 
-	document.querySelector(".button-img-minimize").remove();
-	document.querySelector(".button-img-close").remove();
+	document.querySelector(".button-img-minimize").style.filter = "invert(100%)";
+	document.querySelector(".button-img-close").style.filter = "invert(100%)";
 }
 
 let isFullScreen = false;
@@ -26,14 +26,14 @@ function fullScreenMode() {
 	const mainCont =  document.querySelector("#mainCont");
 
 	if (isFullScreen === false) {
-		sideCont.className = "col s12 animated slideInLeft";
+		sideCont.className = "col s12 animated fadeInLeft";
 		mainCont.style.display = "none";
 		isFullScreen = true;
 		return;
 	}
 
 	else {
-		sideCont.className = "col s3 animated slideInRight";
+		sideCont.className = "col s3 animated fadeInRight";
 		mainCont.style.display = "block";
 		isFullScreen = false;
 		return;
