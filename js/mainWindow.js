1dad8287da1d668c410c1f13d0470d6f4371fbd1
Sanderhelleso <sanderhelleso@hotmail.com>
2018-06-05 18:50:02 +0200

    added comments

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -5,42 +5,56 @@ function start() {
 	const titleBar = require("electron-titlebar");
 	const {ipcRenderer} = electron;
 
+	// get from directory data
 	ipcRenderer.on("directoryFrom:path", function(e, path){
-		console.log(path);
+
+		// create the backup container
 		const backUpDiv = document.createElement("div");
 		backUpDiv.className = "backedUpDir";
 
+		// from dir
 		const fromDir = document.createElement("h5");
 		fromDir.innerHTML = path;
 
+		// icon
 		const icon = document.createElement("i");
 		icon.className = "material-icons renewRunning";
 		icon.innerHTML = "autorenew";
 
+		// append to backup container
 		backUpDiv.appendChild(fromDir);
 		backUpDiv.appendChild(icon);
 
+		// display in main window
 		document.querySelector("#backedUpCont").appendChild(backUpDiv);
 	});
 
+	// get to directory data
 	ipcRenderer.on("directoryTo:path", function(e, path){
-		console.log(path);
+
+		// to dir
 		const toDir = document.createElement("h5");
 		toDir.innerHTML = path;
 
+		// status | RUNNING / STOPPED
 		const status = document.createElement("p");
 		status.className = "status";
 		status.innerHTML = "<i class='material-icons running'>brightness_1</i><span>Running</span>";
 
+		// start button
 		const startBtn = document.createElement("a");
 		startBtn.className = "waves-effect waves-light btn btn-small startBackUpBtn z-depth-2 hoverable";
 		startBtn.innerHTML = "<i class='material-icons left'>backup</i>start";
 
+		// stop button
 		const stopBtn = document.createElement("a");
 		stopBtn.className = "waves-effect waves-light btn btn-small stopBackUpBtn disabledBtn z-depth-2 hoverable";
 		stopBtn.innerHTML = "<i class='material-icons left'>pause</i>stop";
 
+		// get the backup container created in directoryFrom:path
 		const backUpDiv = document.querySelector("#backedUpCont").childNodes[document.querySelector("#backedUpCont").childNodes.length - 1];
+
+		// display in main window
 		backUpDiv.appendChild(toDir);
 		backUpDiv.appendChild(status);
 		backUpDiv.appendChild(startBtn);
